8fd3c887d7bdc7f998a91bd2d0efc0bd64848541
sizukutamago <sizukutamago@gmail.com>
2017-07-21 19:00:35 +0900

    twigの設定

--- public/index.php
+++ public/index.php
@@ -9,8 +9,18 @@ $whoops = new \Whoops\Run();
 $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
 $whoops->register();
 
+//timezoneを日本に
+date_default_timezone_set('Asia/Tokyo');
+
 //.envの読み込み
 $dotenv = new \Dotenv\Dotenv(dirname(__DIR__));
 $dotenv->load();
 
-var_dump(getenv('DB_HOST'));
+//twigの設定
+$basedir = dirname(__DIR__);
+$loader = new \Twig_Loader_Filesystem($basedir . '/view/template');
+$twig = new \Twig_Environment($loader, [
+    'cache' => $basedir . '/cache/twig',
+    'debug' => true,
+]);
+
